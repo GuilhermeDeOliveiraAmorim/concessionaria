e82e3830a34cb9c82e770c3eac94b7ffe0a85816
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-11-20 15:06:23 -0300

    feat: get all information

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,78 @@
+import { api } from "../get-api/axios";
+
+interface IUser {
+	id: number,
+	email: string,
+	password: string,
+	is_available: number
+}
+
+interface ICar {
+	id: number,
+	make: string,
+	model: string,
+	transmission: string,
+	year: number,
+	value: number,
+	is_available: number
+}
+
+interface ISeller {
+	id: number,
+	car_id: number,
+	is_available: number
+}
+
+interface ISale {
+	id: number,
+	car_id: number,
+	seller_id: number,
+	created_at: string,
+	is_available: number
+	car: ICar,
+	seller: ISeller
+}
+
+interface HomeProps {
+	user: IUser;
+	cars: ICar[];
+	sellers: ISeller[];
+	sales: ISale[];
+}
+
+export default function Home(props: HomeProps) {
+	const { user, cars, sellers, sales } = props;
+
+	console.log(user);
+	console.log(cars);
+	console.log(sellers);
+	console.log(sales);
+
+	return (
+		<div>
+			{cars.map(car =>
+				<p key={car.id}>
+					{car.make}
+				</p>
+			)}
+		</div>
+	);
+}
+
+export const getServerSideProps = async () => {
+	const [user, cars, sellers, sales] = await Promise.all([
+		api.get("users/1"),
+		api.get("cars/"),
+		api.get("sellers/"),
+		api.get("sales/"),
+	])
+
+	return {
+		props: {
+			user: user.data,
+			cars: cars.data,
+			sellers: sellers.data,
+			sales: sales.data,
+		}
+	}
+};
